90bcb88071b2a680bbc5969b120bac1bc80d0133
Nathaniel Mosco <natemosco@gmail.com>
2019-11-05 16:51:24 -0500

    created reducers file/folder
    adding state to app

---
+++ reducer-pattern-todo-app/src/reducers/reducers.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+
+// const [list, setList] = useState({
+//     item: 'Learn about reducers',
+//     completed: false,
+//     id: 3892987589
+//   });
+
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+const appReducer = (state, action) => {
+    return <div></div>
+}
+
+export { appReducer, initialState }
