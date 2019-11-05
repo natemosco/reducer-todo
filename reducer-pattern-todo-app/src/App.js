7d32c3f3bb671a5b57e4789585e8ea223bc131ce
Nathaniel Mosco <natemosco@gmail.com>
2019-11-05 17:51:36 -0500

    changed shape of initial state to be an object for reference
    instantiated useContext to passDown data

--- reducer-pattern-todo-app/src/App.js
+++ reducer-pattern-todo-app/src/App.js
@@ -1,13 +1,22 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer, useState, useContext } from 'react';
 
+import FormContext from "./context/FormContext";
 import { initialState, appReducer } from "./reducers/reducers"
+
+import TaskContainer from "./components/TaskContainer";
 import './App.css';
 
 function App() {
   const [state, dispatch] = useReducer(appReducer, initialState);
+  const [formInput, setFormInput] = useState("");
 
+  const
   return (
     <div className="App">
+      <h1>Your Todo List</h1>
+      <FormContext.Provider value={state, dispatch, formInput}>
+        <TaskContainer></TaskContainer>
+      </FormContext.Provider>
 
     </div>
   );
